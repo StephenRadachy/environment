18b1834932334d88d2d74305a78eff64fc6485de
Stephen J Radachy <stephen@radachy.com>
2019-04-22 14:17:52 -0500

    add repo and nv

--- plugins/++/++.plugin.zsh
+++ plugins/++/++.plugin.zsh
@@ -16,3 +16,25 @@ decrypt() {
     echo "USAGE: decrypt <input-file> <output-file>"
   fi
 }
+
+repo() {
+    if [ -n "$1" ] && [ -n "$2" ]; then
+        if [ -d "${HOME}/Code/github.com/$1/$2" ]; then
+            pushd "${HOME}/Code/github.com/$1/$2"
+        elif [ -d "${HOME}/Code/$1/$2" ]; then
+            pushd "${HOME}/Code/$1/$2"
+        else
+            echo "ERROR: cannot find repo"
+        fi
+    else
+        echo "USAGE: repo <org> <reponame>"
+    fi
+}
+
+nv() {
+    case "$1" in
+    "edit-config") repo stephenradachy environment && code . && popd
+        ;;
+    *) echo "see repo for documentation"
+    esac
+}
